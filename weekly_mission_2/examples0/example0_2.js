ff1211a33d6758a679f69745bafdbe90ea283ae1
Jorge Luis Rdz. Medina <94321701+JorgeRodriguez1998@users.noreply.github.com>
2022-04-18 16:23:04 -0500

    Add files via upload

---
+++ weekly_mission_2/examples0/example0_2.js
@@ -0,0 +1,11 @@
+// Ejemplo 2: Crear un objeto con propiedades
+const explorer = {
+    name: "Jorge",
+    age: 23,
+    mission: "Node JS",
+    stack: ["js", "node", "vue", "react"]
+  }
+  console.log("Ejemplo 2: Crear un objeto con propiedades")
+  console.log(explorer.name)
+  console.log(explorer["name"])
+  console.log(explorer)
---
+++ weekly_mission_2/examples0/example0_3.js
@@ -0,0 +1,19 @@
+// Ejemplo 3: Objeto con diferentes propiedades
+const myObject3 = {
+    name: "Juan",
+    age: 22,
+    nicknames: [
+     "Tulipan",
+      "Tulancingo",
+      "Tulish"
+    ],
+    address: {
+      zip_code: "10000",
+      street: "Av. Carranza",
+      city: "SLP"
+    }
+  }
+  console.log("Ejemplo 3: Objeto con diferentes propiedades")
+  console.log(myObject3)
+  console.log(myObject3.name)
+  console.log(myObject3["address"])
